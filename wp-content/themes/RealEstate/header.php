f2804611c11d16a9d617719ddb88a82ef8611dea
Muhammad-Abdullah-10 <59797973+Muhammad-Abdullah-10@users.noreply.github.com>
2024-02-28 23:13:03 +0500

    Show and hide menu on hamburger click functionality added with javascript

--- wp-content/themes/RealEstate/header.php
+++ wp-content/themes/RealEstate/header.php
@@ -11,7 +11,7 @@
 <body <?php body_class(); ?>>
     <!-- HEADER -->
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav navbar-collapse mr-auto">
                     <li class="nav-item">
@@ -39,6 +39,40 @@
                     </li>
                     <li class="nav-item">
                         <button class="get-listing btn btn-primary button-listing">Get Listing</button>
+                    </li>
+                </ul>
+            </div>
+        </nav> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="<?php get_template_directory_uri(); ?>"> <?php if (has_custom_logo()) {
+            // $custom_logo_id = get_theme_mod( 'custom_logo' );
+                 $logo_other_size = wp_get_attachment_image_src(get_theme_mod('custom_logo'), 'thumbnail')[0];
+              ?>
+                <img class="main-logo" src="<?php echo $logo_other_size; ?>" alt="">
+                <?php  } else {
+                echo bloginfo();
+                }  ?></a>
+            <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> -->
+            <button class="ham-btn" data-ham="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse desktop-menu" >
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                    <?php wp_nav_menu(array(
+                            'menu' => 'HeaderOneMain',
+                            'menu_id' => 'main-header-menu'
+                        )); ?>
+                    </li>
+                </ul>
+            </div>
+            <div class="mobile-nav-bar mobile-menu-visibility" id="navbarNavDropdown" data-menushow="false">
+                <ul class="navbar-nav">
+                    <li class="nav-item ">
+                    <?php wp_nav_menu(array(
+                            'menu' => 'HeaderOneMain',
+                            'menu_id' => 'main-header-menu'
+                        )); ?>
                     </li>
                 </ul>
             </div>
--- wp-content/themes/RealEstate/inc/js/index.js
+++ wp-content/themes/RealEstate/inc/js/index.js
@@ -7,7 +7,33 @@
 
 // const nav = new Navi();
 // const app = new Appi();
-window.addEventListener("load", (event) => {
-    let test = document.getElementById("navbarSupportedContent");
-    console.log(test);
+window.addEventListener("load", (e) => {
+    
+    let navMobile = document.querySelector(".ham-btn");
+    
+    let activeMobileMenu =  document.querySelector("#navbarNavDropdown");
+
+    
+  
+    
+    navMobile.addEventListener('click' , (e)=>{
+        let mobileVisibility = activeMobileMenu.getAttribute('data-menushow'); 
+       let dataHam = navMobile.getAttribute('data-ham'); 
+        navMobile.setAttribute( 'data-ham' ,dataHam === "false" ? "true" : "false");
+        if(dataHam === "true" &&  mobileVisibility === "true")
+        {
+            activeMobileMenu.classList.remove('mobile-nav-bar-active');
+            activeMobileMenu.classList.add('mobile-menu-visibility');
+            activeMobileMenu.setAttribute( 'data-menushow' ,"false");
+            
+        }
+        else{
+            activeMobileMenu.classList.add('mobile-nav-bar-active');
+            
+            activeMobileMenu.classList.remove('mobile-menu-visibility');
+            activeMobileMenu.setAttribute( 'data-menushow' ,"true");
+
+        }
+    });
+    
   });
